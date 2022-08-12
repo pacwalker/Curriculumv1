bab1fca7b1c239b778b74d3ad2e75c0f791addce
pacwalker <miguelangelvelazquezcarranza@gmail.com>
2022-08-12 01:27:43 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,10 +19,10 @@ self.addEventListener('install', event=>{
     .then(cache=>{
         return cache.addAll([
             './index.html',
-            /*'/css/animate.css',
+            '/css/animate.css',
             '/css/fontawesome.css',
             '/css/templatemo_misc.css',
-            '/css/templatemo_style.css',*/
+            '/css/templatemo_style.css',
             '/js/app.js'
         ]);
     });
